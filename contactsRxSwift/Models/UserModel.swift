d1deee5ce927c223a33b1e8a060b31ec2a557a21
Pierre Dias <cdias.pierre@gmail.com>
2023-11-21 21:25:42 -0300

    Criado UserModel, UserService

---
+++ contactsRxSwift/Models/UserModel.swift
@@ -0,0 +1,28 @@
+//
+//  UserModel.swift
+//  contactsRxSwift
+//
+//  Created by Pierre Campos Dias on 21/11/23.
+//
+
+import Foundation
+
+
+struct UserResonse: Codable {
+    let results: [User]
+}
+
+struct User: Codable {
+//    let id: ID
+    let name: Name
+}
+
+struct ID: Codable {
+    let type: String
+    let value: String
+}
+
+struct Name: Codable {
+    let first: String
+    let last: String
+}
---
+++ contactsRxSwift/Services/UserService.swift
@@ -0,0 +1,40 @@
+//
+//  UserService.swift
+//  contactsRxSwift
+//
+//  Created by Pierre Campos Dias on 21/11/23.
+//
+
+import Foundation
+import RxSwift
+
+protocol UserServiceProtocol {
+    func callAPI<T: Codable>() -> Observable<T>
+}
+//func callAPI<T>() -> RxSwift.Observable<T> where T : Decodable, T : Encodable {
+class UserService: UserServiceProtocol {
+    let baseURL = URL(string: "https://randomuser.me/api/?page=0&results=20&seed=abc&nat=br")!
+    let session = URLSession(configuration: .default)
+    var dataTask: URLSessionDataTask? = nil
+    
+    
+    func callAPI<T: Codable>() -> Observable<T> {
+        return Observable.create { emitter in
+            self.dataTask = self.session.dataTask(with: self.baseURL, completionHandler: { data, response, error in
+                do {
+                    let model: UserResonse = try JSONDecoder().decode(UserResonse.self, from: data ?? Data())
+                    emitter.onNext(model.results as! T)
+                } catch let error {
+                    emitter.onError(error)
+                }
+                emitter.onCompleted()
+            })
+            self.dataTask?.resume()
+            return Disposables.create {
+                self.dataTask?.cancel()
+            }
+        }
+    }
+    
+    
+}
